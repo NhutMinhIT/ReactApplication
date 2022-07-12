088ce0f50752bb9c7e45b0f3398eb6812c95d600
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2022-07-12 15:02:46 +0700

    Redux Thunk

--- src/components/DishdetailComponent.jsx
+++ src/components/DishdetailComponent.jsx
@@ -7,6 +7,7 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -32,8 +33,7 @@ class CommentForm extends Component {
 	handleSubmit(values) {
 		this.toggleModal();
 		this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
-		// console.log("Current State is: " + JSON.stringify(values));
-		// alert("Current State is: " + JSON.stringify(values));
+
 	}
 
 	render() {
@@ -140,7 +140,7 @@ function RenderComments({ comments, addComment, dishId }) {
 						);
 					})}
 				</ul>
-				{/* <CommentForm /> */}
+
 				<CommentForm dishId={dishId} addComment={addComment} />
 			</div>
 		);
@@ -152,34 +152,28 @@ function RenderComments({ comments, addComment, dishId }) {
 	}
 }
 
-// function RenderComments({comments}) {
-// if (comments != null) {
-// return (
-// <div className="col-12 col-md-5 m-1">
-// <h4>Comments</h4>
-// <ul className="list-unstyled">
-// {comments.map((comment) => {
-// return (
-// <li key={comment.id}>
-// <p>{comment.comment}</p>
-// <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-// </li>
-// );
-// })}
-// </ul>
-// </div>
-// );
-// }
-// else {
-// return (
-// <div></div>
-// );
-// }
 
-// }
 
 const DishDetail = (props) => {
-	if (props.dish != null) {
+	if (props.isLoading) {
+		return (
+			<div className="container">
+				<div className="row">
+					<Loading />
+				</div>
+			</div>
+		);
+	}
+	else if (props.errMess) {
+		return (
+			<div className="container">
+				<div className="row">
+					<h4>{props.errMess}</h4>
+				</div>
+			</div>
+		);
+	}
+	else if (props.dish != null) {
 		return (
 			<div className="container">
 				<div className="row">
@@ -189,14 +183,14 @@ const DishDetail = (props) => {
 						<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
 					</Breadcrumb>
 					<div className="col-12">
-						{/* <h3 class="text-info">Menu</h3> */}
+
 						<h3 class="text-secondary">Menu</h3>
 						<hr />
 					</div>
 				</div>
 				<div className="row  text-dark text-justify">
 					<RenderDish dish={props.dish} />
-					{/* <RenderComments comments={props.comments}/> */}
+
 					<RenderComments comments={props.comments}
 						addComment={props.addComment}
 						dishId={props.dish.id}
--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
-import {
-   Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Button,
-   Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label
-} from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
-
    constructor(props) {
       super(props);
 
@@ -15,16 +11,14 @@ class Header extends Component {
          isModalOpen: false
       };
 
+
       this.toggleNav = this.toggleNav.bind(this);
       this.toggleModal = this.toggleModal.bind(this);
       this.handleLogin = this.handleLogin.bind(this);
-
    }
 
    toggleNav() {
-      this.setState({
-         isNavOpen: !this.state.isNavOpen
-      });
+      this.setState({ isNavOpen: !this.state.isNavOpen });
    }
 
    toggleModal() {
@@ -38,12 +32,13 @@ class Header extends Component {
       alert("Username: " + this.username.value + " Password: " + this.password.value
          + " Remember: " + this.remember.checked);
       event.preventDefault();
+
    }
+
 
    render() {
       return (
          <React.Fragment>
-
             <Navbar dark expand="md" >
                <div className="container">
                   <div className='row'>
@@ -76,16 +71,14 @@ class Header extends Component {
                         </Nav>
                         <Nav className="ml-auto" navbar>
                            <NavItem>
-                              <Button outline onClick={this.toggleModal}>
-                                 <span className="fa fa-sign-in fa-lg"></span> Login
-                              </Button>
+                              <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
                            </NavItem>
                         </Nav>
+
                      </Collapse>
                   </div>
                </div>
             </Navbar>
-
             <div class="jumbotron">
                <div className="container">
                   <div className="row row-header">
