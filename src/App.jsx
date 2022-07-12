30a9665bb0859701d8fa0708e7beaf4d92d34d20
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2022-07-12 13:53:09 +0700

    React Redux Form Validation

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,19 @@ import './App.css';
 const store = ConfigureStore();
 
 class App extends Component {
-  
+
   render() {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          {/* <div className="App"> */}
-          <div>
+          <div className='App'>
             <Main />
           </div>
         </BrowserRouter>
       </Provider>
-      // <BrowserRouter>
-      //   <div className="App">
-      //     <Main />
-      //   </div>
-      // </BrowserRouter>
+
     );
-  }  
+  }
 
 }
 
--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-//import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
-import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Button, 
-   Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
+import {
+   Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Button,
+   Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label
+} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
 
    constructor(props) {
       super(props);
-      
+
       this.state = {
          isNavOpen: false,
          isModalOpen: false
-     };
-      
+      };
+
       this.toggleNav = this.toggleNav.bind(this);
       this.toggleModal = this.toggleModal.bind(this);
       this.handleLogin = this.handleLogin.bind(this);
 
    }
-   
+
    toggleNav() {
       this.setState({
-         isNavOpen: !this.state.isNavOpen 
+         isNavOpen: !this.state.isNavOpen
       });
    }
 
@@ -35,7 +36,7 @@ class Header extends Component {
    handleLogin(event) {
       this.toggleModal();
       alert("Username: " + this.username.value + " Password: " + this.password.value
-            + " Remember: " + this.remember.checked);
+         + " Remember: " + this.remember.checked);
       event.preventDefault();
    }
 
@@ -49,11 +50,11 @@ class Header extends Component {
                      <NavbarToggler onClick={this.toggleNav} />
                      <NavbarBrand>
                         <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
-                     </NavbarBrand>                     
+                     </NavbarBrand>
                      <Collapse isOpen={this.state.isNavOpen} navbar>
                         <Nav navbar>
                            <NavItem>
-                              <NavLink  NavLink className="nav-link" to="/home">
+                              <NavLink NavLink className="nav-link" to="/home">
                                  <span className="fa fa-home fa-lg"></span> Home
                               </NavLink>
                            </NavItem>
@@ -80,20 +81,20 @@ class Header extends Component {
                               </Button>
                            </NavItem>
                         </Nav>
-                     </Collapse>       
+                     </Collapse>
                   </div>
                </div>
-            </Navbar> 
+            </Navbar>
 
             <div class="jumbotron">
                <div className="container">
                   <div className="row row-header">
                      <div className="col-12 col-sm-6">
                         <h1>Ristorante Con Fusion</h1>
-                        <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>                       
+                        <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                      </div>
                   </div>
-               </div>              
+               </div>
             </div>
 
             <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
@@ -102,18 +103,18 @@ class Header extends Component {
                   <Form onSubmit={this.handleLogin}>
                      <FormGroup>
                         <Label htmlFor="username">Username</Label>
-                           <Input type="text" id="username" name="username"
-                              innerRef={(input) => this.username = input} />
+                        <Input type="text" id="username" name="username"
+                           innerRef={(input) => this.username = input} />
                      </FormGroup>
                      <FormGroup>
                         <Label htmlFor="password">Password</Label>
-                           <Input type="password" id="password" name="password"
-                              innerRef={(input) => this.password = input}  />
+                        <Input type="password" id="password" name="password"
+                           innerRef={(input) => this.password = input} />
                      </FormGroup>
                      <FormGroup check>
                         <Label check>
                            <Input type="checkbox" name="remember"
-                              innerRef={(input) => this.remember = input}  />
+                              innerRef={(input) => this.remember = input} />
                            Remember me
                         </Label>
                      </FormGroup>
